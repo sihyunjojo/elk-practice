01309d097e29211e2e6b9530f22fc6b8ee1cec4f
sihyunjojo <si4018@naver.com>
2024-09-01 22:06:02 +0900

    feat: basic elk stack

---
+++ src/test/java/com/elk/demo/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
